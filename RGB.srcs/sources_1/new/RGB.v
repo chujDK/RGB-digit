747f3bd407b9a9e2a47d87e5ba9ce4d89ef5691e
chujDK <1768485949@qq.com>
2021-12-22 19:00:58 +0800

    first commit

---
+++ RGB.srcs/sources_1/new/RGB.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/12/22 18:57:30
+// Design Name: 
+// Module Name: RGB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module RGB(
+
+    );
+endmodule
