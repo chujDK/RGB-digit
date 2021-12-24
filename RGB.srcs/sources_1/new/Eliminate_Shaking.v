7f4ed9b6a688cfe75b85fcd733b4c2d3bcb96033
StarlitRover <747712608@qq.com>
2021-12-24 22:09:22 +0800

    Eliminate shaking Done

---
+++ RGB.srcs/sources_1/new/Eliminate_Shaking.v
@@ -0,0 +1,48 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/12/24 19:07:16
+// Design Name: 
+// Module Name: Eliminate_Shaking
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Eliminate_Shaking(
+    input key,clk,rst_,
+    output key_pulse
+    );
+
+    wire CP;
+    divider #(.WIDTH(20),.N(1000000)) div(clk,1,CP);
+    reg [2:0] ST;
+    parameter S0 = 3'b0,S1 = 3'b001,S2 = 3'b010,S3 = 3'b011,S4 = 3'b100,S5 = 3'b101;
+    always @(posedge CP or negedge rst_) 
+    begin
+        if(!rst_)
+            ST<=key?S3:S0;
+        else
+            case(ST)
+            S0: ST<=key?S1:S0;
+            S1: ST<=key?S3:S2;
+            S2: ST<=key?S1:S0;
+            S3: ST<=key?S3:S4;
+            S4: ST<=key?S5:S0;
+            S5: ST<=key?S3:S4;
+            default: ST<=key?S3:S0;
+            endcase
+    end
+    assign key_pulse=((ST==S3)||(ST==S4)||(ST==S5));
+endmodule
---
+++ RGB.srcs/sources_1/new/Top.v
@@ -0,0 +1,38 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2021/12/24 20:56:29
+// Design Name: 
+// Module Name: Top
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Top(
+    input [3:0] Duration,
+    input [1:0] color,
+    input SET,EN,clk,
+    output [7:0] AN,DP,
+    output Green,Red,Yellow
+    );
+
+    wire [1:0] color_pulse;
+    wire SET_pulse,EN_pulse;
+    Eliminate_Shaking ES1(.key(color[0]),.clk(clk),.key_pulse(color_pulse[0])),
+                      ES2(.key(color[1]),.clk(clk),.key_pulse(color_pulse[1])),
+                      ES3(.key(SET),.clk(clk),.key_pulse(SET_pulse)),
+                      ES4(.key(EN),.clk(clk),.key_pulse(EN_pulse));
+    RGB rgb(Duration, color_pulse, SET_pulse, EN_pulse, clk, AN, DP, Green, Red, Yellow);
+endmodule
